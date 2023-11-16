44a0b5dcb537705169aac43974bfe8eaf92bd380
GianniLunardi <gianni.lunardi96@gmail.com>
2023-11-16 07:17:04 +0100

    test files to check naive controllers works and dynamics check (TO BE CLEANED)

--- tests/test_integration.py
+++ tests/test_integration.py
@@ -1,14 +1,17 @@
 import numpy as np
 import matplotlib.pyplot as plt
 from scipy.stats import qmc
-import pendulum_conf as conf
+from safe_mpc.parser import Parameters
 from safe_mpc.model import TriplePendulumModel
 from safe_mpc.abstract import SimDynamics
 from safe_mpc.controller import NaiveController
+from debug import Debug
 
+conf = Parameters('../config/params.yaml')
 model = TriplePendulumModel(conf)
 simulator = SimDynamics(conf, model)
-ocp = NaiveController(conf, model)
+ocp = NaiveController(conf, model, simulator)
+debugger = Debug(conf, ocp)
 
 test_num = 100
 sampler = qmc.Halton(d=ocp.ocp.dims.nu, scramble=False)
@@ -21,6 +24,7 @@ t = np.arange(0, conf.T, conf.dt)
 x_ref = np.array([conf.q_max - 0.05, np.pi, np.pi, 0, 0, 0])
 ocp.setReference(x_ref)
 
+
 def init_guess(p):
     x0 = np.zeros((model.nx,))
     x0[:model.nq] = x0_vec[p]
@@ -28,44 +32,24 @@ def init_guess(p):
     ocp.initialize(x0)
     return ocp.getGuess()
 
-def plot_trajectory(x, x_sim):
-    fig, ax = plt.subplots(3, 1, sharex='col')
-    ax[0].plot(t, x[:-1, 0], label='q1')
-    ax[0].plot(t, x_sim[:-1, 0], label='q1_sim', linestyle='--')
-    ax[0].legend()
-    ax[1].plot(t, x[:-1, 1], label='q2')
-    ax[1].plot(t, x_sim[:-1, 1], label='q2_sim', linestyle='--')
-    ax[1].legend()
-    ax[2].plot(t, x[:-1, 2], label='q3')
-    ax[2].plot(t, x_sim[:-1, 2], label='q3_sim', linestyle='--')
-    ax[2].legend()
-    ax[2].set_xlabel('time [s]')
 
 def simulate(x0, u):
-    x_sim = np.empty((conf.N + 1, model.nx)) * np.nan
-    x_sim[0] = x0
+    x_sim = np.empty((conf.n_steps + 1, model.nx)) * np.nan
+    x_sim[0, :3] = x0
+    x_sim[0, 3:] = np.zeros(3)
 
-    for j in range(conf.N):
-        x_sim[j + 1] = simulator.simulate(x_sim[j], u[j])
+    for k in range(conf.N):
+        x_sim[k + 1] = np.copy(simulator.simulate(x_sim[k], u[k]))
     return x_sim
 
-res = []
-for i in range(test_num):
-    res.append(init_guess(i))
-x_guess_vec, u_guess_vec = zip(*res)
 
-res_sim = []
+x_guess_vec, u_guess_vec = [], []
 for i in range(test_num):
-    x0 = np.zeros((model.nx,))
-    x0[:model.nq] = x_guess_vec[i][0,:3]    #x0_vec[i]
-    res_sim.append(simulate(x0, u_guess_vec[i]))
-x_sim_vec = np.array(res_sim)
+    xg, ug = init_guess(i)
+    x_guess_vec.append(xg)
+    u_guess_vec.append(ug)
 
 for j in range(test_num):
-    plot_trajectory(x_guess_vec[j], x_sim_vec[j])
-    # print('i = ', j, ', x0 = ', x0_vec[j])
-    plt.savefig('plots/test_integration' + str(j) + '.png')
-    plt.close()
-
-
-
+    x_roll = simulate(x0_vec[j], u_guess_vec[j])
+    debugger.plotTrajectory(j, x0_vec[j], x_guess_vec[j], x_roll)
+    # print('Test number: ', j, ', Diff norm ', np.linalg.norm(x_roll[:conf.N+1] - x_guess_vec[j]))
--- tests/test_naive.py
+++ tests/test_naive.py
@@ -1,24 +1,28 @@
 # import time
-import pickle
+# import pickle
 import numpy as np
 from scipy.stats import qmc
-import pendulum_conf as conf
+from safe_mpc.parser import Parameters
 from safe_mpc.model import TriplePendulumModel
 from safe_mpc.abstract import SimDynamics
 from safe_mpc.controller import NaiveController
+from debug import Debug
 
+conf = Parameters('../config/params.yaml')
 model = TriplePendulumModel(conf)
 simulator = SimDynamics(conf, model)
-ocp = NaiveController(conf, model)
+ocp = NaiveController(conf, model, simulator)
 
-data_dir = '../data/'
-data_no = pickle.load(open(data_dir + "results_no_constraint.pkl", 'rb'))
+t_g = np.arange(0, conf.T, conf.dt)
+t_s = np.arange(0, conf.dt * conf.n_steps, conf.dt)
 
-test_num = 100
+# data_no = pickle.load(open(conf.DATA_DIR + "results_no_constraint.pkl", 'rb'))
+
 sampler = qmc.Halton(d=ocp.ocp.dims.nu, scramble=False)
-sample = sampler.random(n=test_num)
-l_bounds = model.x_min[:model.nq]
-u_bounds = model.x_max[:model.nq]
+sample = sampler.random(n=conf.test_num)
+eps = 1e-5
+l_bounds = model.x_min[:model.nq] + eps
+u_bounds = model.x_max[:model.nq] - eps
 x0_vec = qmc.scale(sample, l_bounds, u_bounds)
 # x0_vec = data_no['x0_vec']
 
@@ -42,7 +46,7 @@ def simulate(p):
     u = np.empty((conf.n_steps, model.nu)) * np.nan
     x_sim[0] = x0
 
-    controller.setGuess(np.copy(x_guess_vec[p]), np.copy(u_guess_vec[p]))
+    controller.setGuess(x_guess_vec[p], u_guess_vec[p])
 
     j = 0
     for j in range(conf.n_steps):
@@ -50,30 +54,32 @@ def simulate(p):
         x_sim[j + 1] = simulator.simulate(x_sim[j], u[j])
         if not model.checkStateConstraints(x_sim[j + 1]):
             break
-    return j
+
+    debugger.plotTrajectory(p, x0_vec[p], x_guess_vec[p], x_sim)
+    return j, x_sim
 
 
-res = []
-for i in range(test_num):
-    res.append(init_guess(i))
-x_guess_vec, u_guess_vec = zip(*res)
+x_guess_vec, u_guess_vec = [], []
+for i in range(conf.test_num):
+    xg, ug = init_guess(i)
+    x_guess_vec.append(xg)
+    u_guess_vec.append(ug)
 
-print('Init guess success: ' + str(ocp.success) + ' over ' + str(test_num))
+print('Init guess success: ' + str(ocp.success) + ' over ' + str(conf.test_num))
 
 del ocp
 conf.solver_type = 'SQP_RTI'
-controller = NaiveController(conf, model)
+controller = NaiveController(conf, model, simulator)
+debugger = Debug(conf, controller)
+controller.setReference(x_ref)
 
-res_steps = np.zeros(test_num)
-for i in range(test_num):
-    res_steps[i] = simulate(i)
+res = []
+for i in range(conf.test_num):
+    res.append(simulate(i))
+res_steps, x_sim_vec = zip(*res)
+res_steps = np.array(res_steps)
 
 print('Residual steps:\n', res_steps)
 print('Mean residual steps: ', np.mean(res_steps))
 
-x_test = data_no['x_guess_vec']
-diff_norm = np.zeros(test_num)
-for i in range(test_num):
-    diff_norm[i] = np.linalg.norm(x_guess_vec[i] - x_test[i])
-
-# print('diff norm: ', diff_norm)
+print(np.where(res_steps < controller.N)[0].size)
