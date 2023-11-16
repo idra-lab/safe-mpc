f40ac0d969134c64ca023f6700d8dbeb6c365f84
GianniLunardi <gianni.lunardi96@gmail.com>
2023-11-16 07:17:25 +0100

    [add] debug class

---
+++ tests/debug.py
@@ -0,0 +1,37 @@
+import os
+import numpy as np
+import matplotlib.pyplot as plt
+
+
+class Debug:
+    def __init__(self, conf, controller):
+        self.horizon = np.arange(0, conf.N * conf.dt, conf.dt)
+        self.tot_time = np.arange(0, conf.n_steps * conf.dt, conf.dt)
+        self.conf = conf
+        self.controller = controller
+        # Create the temporary directories for debugging purposes
+        cwd = os.getcwd()
+        self.plot_dir = os.path.join(cwd, 'plots')
+        if not os.path.exists(self.plot_dir):
+            os.makedirs(self.plot_dir)
+
+    def plotBounds(self, t, ax):
+        ax.plot(t, self.conf.q_max * np.ones_like(t), color='red', linestyle='--')
+        ax.plot(t, self.conf.q_min * np.ones_like(t), color='green', linestyle='--')
+
+    def plotTrajectory(self, test_num, x0, x_guess, x_sim=None):
+        fig, ax = plt.subplots(3, 1, sharex='col')
+        for i in range(3):
+            if x_sim is not None:
+                self.plotBounds(self.tot_time, ax[i])
+                ax[i].plot(self.tot_time, x_sim[:-1, i], label='q' + str(i + 1), color='darkblue',
+                           linestyle='--', linewidth=2)
+            else:
+                self.plotBounds(self.horizon, ax[i])
+            ax[i].plot(self.horizon, x_guess[:-1, i], label='q_g' + str(i + 1), color='darkgreen', linewidth=1.5)
+            ax[i].scatter(x=0, y=x0[i], c='r')
+            ax[i].legend()
+            ax[i].set_ylabel('q' + str(i + 1) + ' (rad)')
+        ax[2].set_xlabel('Time (s)')
+        plt.savefig(self.plot_dir + '/pos' + str(test_num) + '.png')
+        plt.close()
