c0c6de8d379b0c4e0181a76bacac55db91aebada
GianniLunardi <gianni.lunardi96@gmail.com>
2023-12-12 16:48:39 +0100

    compute the 99 quantile (maybe should be changes into max)

--- tests/test_abort.py
+++ tests/test_abort.py
@@ -21,15 +21,18 @@ ocp = SafeBackupController(simulator)
 
 x_viable = np.load(conf.DATA_DIR + '/viable/x_viable.npy')
 nn_out = evaluate_nn(x_viable)
-# print(nn_out)
+print(nn_out)
 
 n = x_viable.shape[0]
 success = np.zeros(n)
+t_comp = []
 for i in range(n):
     ocp.setGuess(np.full((conf.N + 1, model.nx), x_viable[i]), np.zeros((conf.N, model.nu)))
     status = ocp.solve(x_viable[i])
     print(status)
     if status == 0:
+        t_comp.append(ocp.ocp_solver.get_stats('time_tot')[0])
         success[i] = 1
 
 print('Abort: ' + str(np.sum(success)) + ' over ' + str(n))
+print('99% quantile time: ', np.quantile(np.asarray(t_comp), 0.99))
