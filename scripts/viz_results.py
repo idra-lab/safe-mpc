332ba96d7092aea93a3d4e27cfab13f5f71736d2
EliasFontanari <elias.fontanari@unitn.it>
2025-05-05 18:22:37 +0200

    Added cost update to tracking 8

--- scripts/viz_results.py
+++ scripts/viz_results.py
@@ -13,10 +13,10 @@ RENDER_CAPS = True
 params = Parameters('z1', True)
 params.build = False
 model = AdamModel(params)
-#cost = TrackingMovingCircleNLS(model,params.Q_weight,params.R_weight)
-cost = ReachTargetNLS(model,params.Q_weight,params.R_weight)
+cost = TrackingMovingCircleNLS(model,params.Q_weight,params.R_weight)
+#cost = ReachTargetNLS(model,params.Q_weight,params.R_weight)
 robot = NaiveController(model)
-robot.track_traj = False
+robot.track_traj = True
 rviz = RobotVisualizer(params, 4)
 if not(robot.track_traj):
     rviz.setTarget(params.ee_ref)
@@ -24,7 +24,7 @@ if not(robot.track_traj):
 if robot.model.params.obstacles != None:
     rviz.addObstacles(robot.model.params.obstacles)
 
-data = pickle.load(open(f'{params.DATA_DIR}z1_parallel2_use_netTrue_15hor_10sm_mpc.pkl', 'rb'))
+data = pickle.load(open(f'{params.DATA_DIR}z1_naive_use_netNone_45hor_10sm_traj_trackmpc.pkl', 'rb'))
 
 #data = pickle.load(open(f'{params.DATA_DIR}x_traj_opt.pkl','rb'))
 #x=data
@@ -32,7 +32,7 @@ data = pickle.load(open(f'{params.DATA_DIR}z1_parallel2_use_netTrue_15hor_10sm_m
 x = data['x']
 
 time.sleep(1)
-for j in range(0,params.test_num if not(robot.track_traj) else 1):
+for j in range(0,params.test_num):
     print(f"Trajectory {j + 1}")
     rviz.display(x[j][0, :model.nq])
     time.sleep(1)
